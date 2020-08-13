0e688b4816569220d7568a9dafa7a1de251e30b8
Polina <lisurin111@gmail.com>
2020-08-13 16:51:09 +0500

    P-6.1: Пробная версия кода

---
+++ homework/Section06/6-1.py
@@ -0,0 +1,11 @@
+# Дорабоать функцию ask_number(), добавив новый параметр - шаг. Шаг должен быть = 1.
+
+
+def ask_number(question, low, high, step):
+    response = None
+    while response not in range(low, high, step):
+        response = int(input(question).lower())
+    return response
+
+
+ask_number(question='Какой-то вопрос', low=0, high=9, step=1)
